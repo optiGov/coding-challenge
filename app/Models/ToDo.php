b4306a6de377a53d178317642fe5e403dee44558
Julius Türich <info@joonlabs.com>
2022-08-18 10:03:52 +0200

    add models

---
+++ app/Models/ToDo.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ToDo extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * @var string
+     */
+    protected $table = "todo";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'category_id'
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
